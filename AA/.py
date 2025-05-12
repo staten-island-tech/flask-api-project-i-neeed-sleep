018e0d5d0db6e7b3334c365e02f9eb6625e18b3e
i-neeed-sleep <kammyl@nycstudents.net>
2025-05-12 09:25:35 -0400

    eeeeeeeeee

--- AA/.py
+++ AA/.py
@@ -15,11 +15,85 @@ def index():
         url = spell['school']
         parts = url.strip("/").split("/")
         school = parts[-1]
+        part = url.strip("/").split("/")
+        id = part[-1]
         req = []
+
+        if spell["material"] == True:
+            req.append("M")
+        if spell["somatic"] == True:
+            req.append("S")
+        if spell["verbal"] == True:
+            req.append("V")
+
+        if spell["concentration"] == True:
+            c = "Concentration"
+        else:
+            c = ""
+        
+        if spell["ritual"] == True:
+            r = "Ritual"
+        else:
+            r = ""
         
         spells.append({
             'name': spell['name'].capitalize(),
             'lvl': spell['level'],
             'school': school.capitalize(),
-            'req' : req
+            'req' : req,
+            'concentration': c
+            'ritual' : r
+            'id' = id
         })
+
+    render_template("index.html", spells=spells)
+
+@app.route("/spell-details/<id>")
+def spell_detail(id):
+    response = requests.get(f"https://api.open5e.com/v2/spells/{id}")
+    data = response.json()
+    
+    types = [t['type']['name'] for t in data['types']]
+    duration = data.get('duration')
+    target = data.get('target_type')
+    desc = data.get('desc')
+    school = data.get('school')
+    name = data.get('name').capitalize()
+    
+    stat_names = [stat['stat']['name'] for stat in data['stats']]
+    stat_values = [stat['base_stat'] for stat in data['stats']]
+    
+    req = []
+
+    if data["material"] == True:
+        req.append("M")
+    if data["somatic"] == True:
+        req.append("S")
+    if data["verbal"] == True:
+        req.append("V")
+
+    if data["concentration"] == True:
+        c = "Concentration"
+    else:
+        c = ""
+    
+    if data["ritual"] == True:
+        r = "Ritual"
+    else:
+        r = ""
+
+    render_template("spell.html", spell={
+        'name': name,
+        'desc': desc,
+        'target': target,
+        'duration': duration,
+        'lvl': data['level'],
+        'school': school.capitalize(),
+        'req' : req,
+        'concentration': c,
+        'ritual' : r,
+        'desc': desc,
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
